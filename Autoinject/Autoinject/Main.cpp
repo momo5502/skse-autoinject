1564eb5865b5b79eb1f50769d4b064cb0ee1e58e
Maurice Heumann <MauriceHeumann@gmail.com>
2015-09-01 22:49:04 +0200

    Attach console in debug mode, to see if SKSE got injected.

--- Autoinject/Autoinject/Main.cpp
+++ Autoinject/Autoinject/Main.cpp
@@ -4,6 +4,15 @@ BOOL APIENTRY DllMain( HMODULE hModule,DWORD  ul_reason_for_call,LPVOID lpReserv
 {
 	if (DLL_PROCESS_ATTACH)
 	{
+#ifdef _DEBUG
+		FILE* fp;
+		AllocConsole();
+		AttachConsole(GetCurrentProcessId());
+		if (!freopen_s(&fp, "CONOUT$", "w", stdout)) fclose(fp);
+		if (!freopen_s(&fp, "CONOUT$", "w", stderr)) fclose(fp);
+		if (!freopen_s(&fp, "CONIN$", "r", stdin))   fclose(fp);
+#endif
+
 		// Simply load the SKSE library
 		// Main work is done in SDLLP
 		// It basically installs a proxy for dinput8.dll
